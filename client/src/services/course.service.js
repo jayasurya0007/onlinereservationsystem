cfb6ab2d00a85a7ef853609f1247312c84b7c452
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-03-29 18:07:07 +0530

    change auth.js & service.js
    updated the api_url

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://onlinereservationsystem.onrender.com/api/courses";
 
 const CourseService = {
   post(title, description, price) {
